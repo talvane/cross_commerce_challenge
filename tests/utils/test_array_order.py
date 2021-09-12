e291cd0d7d29300227a6eeefce0d44dc9d913ce7
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-09-12 10:53:20 -0300

    fix test

--- tests/utils/test_array_order.py
+++ tests/utils/test_array_order.py
@@ -4,6 +4,7 @@ from api.utils.array_order import quickSort
 aArray = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 aOrderArray = [17, 20, 26, 31, 44, 54, 55, 77, 93]
 
-quickSort(aArray=aArray)
 
-assert aOrderArray == aArray
+def test_quicksort():
+    quickSort(aArray=aArray)
+    assert aOrderArray == aArray
